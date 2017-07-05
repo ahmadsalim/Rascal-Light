b1a0d5bb39ead1700eb3e180f206676ba2c9306d
Ahmad Salim Al-Sibahi <asal@itu.dk>
2017-07-05 14:29:02 +0200

    Fixed broken parser test

--- src/test/scala/RascalWrapperParsingTests.scala
+++ src/test/scala/RascalWrapperParsingTests.scala
@@ -29,17 +29,18 @@ class RascalWrapperParsingTests extends FlatSpec with Matchers {
   }
 
   "The wrapped parser" should "parse Expr.rscli correctly" in {
-    val expected = ModuleDef(
-      List(DataDef("Expr", List(ConstructorDef("var", List(Parameter(BaseType(StringType), "nm"))),
-                                ConstructorDef("cst", List(Parameter(BaseType(IntType), "vl"))),
-                                ConstructorDef("mult", List(Parameter(DataType("Expr"), "el"), Parameter(DataType("Expr"), "er"))))),
-           FunDef(DataType("Expr"), "simplify", List(Parameter(DataType("Expr"), "expr")),
-             VisitExpr(BottomUp, VarExpr("expr"),
-               List(Case(ConstructorPatt("mult", List(ConstructorPatt("cst", List(BasicPatt(IntLit(0)))), VarPatt("y"))), ConstructorExpr("cst", List(BasicExpr(IntLit(0))))),
-                    Case(ConstructorPatt("mult", List(VarPatt("x"), ConstructorPatt("cst", List(BasicPatt(IntLit(0)))))), ConstructorExpr("cst", List(BasicExpr(IntLit(0))))),
-                    Case(ConstructorPatt("mult", List(ConstructorPatt("cst", List(BasicPatt(IntLit(1)))), VarPatt("y"))), VarExpr("y")),
-                    Case(ConstructorPatt("mult", List(VarPatt("x"), ConstructorPatt("cst", List(BasicPatt(IntLit(1)))))), VarExpr("x")))))
-      ))
+    val expected = ModuleDef(List(
+      DataDef("Nat",List(ConstructorDef("zero",List()), ConstructorDef("suc",List(Parameter(DataType("Nat"),"pre"))))),
+      DataDef("Expr",List(ConstructorDef("var",List(Parameter(BaseType(StringType),"nm"))),
+                          ConstructorDef("cst",List(Parameter(DataType("Nat"),"vl"))),
+                          ConstructorDef("mult",List(Parameter(DataType("Expr"),"el"), Parameter(DataType("Expr"),"er"))))),
+      FunDef(DataType("Expr"), "simplify", List(Parameter(DataType("Expr"), "expr")),
+               VisitExpr(BottomUp,VarExpr("expr"), List(
+                 Case(ConstructorPatt("mult",List(ConstructorPatt("cst",List(ConstructorPatt("zero",List()))), VarPatt("y"))),
+                             ConstructorExpr("cst",List(ConstructorExpr("zero",List())))),
+                 Case(ConstructorPatt("mult", List(VarPatt("x"), ConstructorPatt("cst",List(ConstructorPatt("zero",List()))))),ConstructorExpr("cst",List(ConstructorExpr("zero",List())))),
+                 Case(ConstructorPatt("mult",List(ConstructorPatt("cst",List(ConstructorPatt("suc",List(ConstructorPatt("zero",List()))))), VarPatt("y"))),VarExpr("y")),
+                 Case(ConstructorPatt("mult",List(VarPatt("x"), ConstructorPatt("cst",List(ConstructorPatt("suc",List(ConstructorPatt("zero",List()))))))),VarExpr("x")))))))
     parseAndTranslateMatchingExpected("Expr.rscli", expected)
   }
 
